305926d56707d0de3bc90a3caae67c5fe68cf15f
Ueslei Sales <wesleijt@hotmail.com>
2022-10-29 16:22:50 +0000

    criação das regras de category e company

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Http\Requests\StoreUpdateCategory;
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{   
+    protected $repository;
+
+    public function __construct(Category $category)
+    {
+        $this->repository = $category;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   $categories = $this->repository->get();
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUpdateCategory $request)
+    {
+        $category = $this->repository->create($request->validated());
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($url)
+    {
+        $category = $this->categoryRepository->where('url', $url)->firstOrFail();
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreUpdateCategory $request, $url)
+    {
+        $category = $this->repository->where('url', $url)->firstOrFail();
+        $category->update($request->validated());
+
+        return response()->json(['message' => 'success']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($url)
+    {
+        $category = $this->repository->where('url', $url)->firstOrFail();
+        $category->delete();
+
+        return response()->json(['message' => 'Deleted'], 204);
+    }
+}
---
+++ app/Http/Requests/StoreUpdateCategory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateCategory extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $url = $this->segment(2); //pegando o valor do 2 parâmetro pela url
+        // $url = $this->url;
+
+        return [
+            'title' => "required|min:3|max:150|unique:categories,title,{$url},url",
+            'description' => 'required|min:3|max:255'
+        ];
+    }
+}
